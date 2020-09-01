626645acffcf8b069b2397824876b639a6413e3a
Hanghang Liu <nate.hanghang.liu@gmail.com>
2020-09-01 10:35:53 +0800

    initial commit of code base

---
+++ digest.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/nlopes/slack"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+var re = regexp.MustCompile(`(?m)<@(\w+)>`)
+
+const initialNumBufSize = 24
+
+func DigestTitle() string {
+	loc, _ := time.LoadLocation("America/Los_Angeles")
+	t := time.Now()
+	t = t.In(loc)
+	return fmt.Sprintf("Apache Gobblin Daily Email Digest (%s)", t.Format("2006-01-02"))
+}
+
+func RunDailyDigest(c *Config) string {
+	if c.From == "" || c.To == "" || c.SendgridToken == "" {
+		fmt.Println("Some config is missing. Please double check `FROM/TO/SENDGRID_TOKEN`.")
+		return "Some config is missing. Please double check `FROM/TO/SENDGRID_TOKEN`."
+	}
+
+	// Initialize slack api
+	api := slack.New(c.SlackAppToken)
+
+	// Fetch user list
+	userList, err := Users(api)
+	if err != nil {
+		fmt.Println("Failed to fetch the user list: " + err.Error())
+		return "Failed to fetch the user list: " + err.Error()
+	}
+
+	// Fetch channels
+	pm := &slack.GetConversationsParameters{
+		ExcludeArchived: "true",
+		Limit:           1000,
+	}
+	channels, _, err := api.GetConversations(pm)
+	if err != nil {
+		fmt.Println("Failed to fetch the user list: " + err.Error())
+		return "Failed to fetch the user list: " + err.Error()
+	}
+
+	// Construct conversation history html content
+	buffer := bytes.NewBuffer(make([]byte, 0, initialNumBufSize))
+	for _, channel := range channels {
+		if channel.Name == "daily-digest" {
+			continue
+		}
+
+		ch := &slack.GetConversationHistoryParameters{
+			ChannelID: channel.ID,
+			Oldest:    fmt.Sprintf("%f", float64(time.Now().Add(-48*time.Hour).Unix())),
+			Latest:    fmt.Sprintf("%f", float64(time.Now().Unix())),
+			Limit:     10000,
+		}
+
+		history, err := api.GetConversationHistory(ch)
+		if err != nil {
+			log.Println("Failed to get conversation history: ", channel.Name)
+		}
+		if len(history.Messages) > 0 {
+			buffer.WriteString(fmt.Sprintf("<h3><u>#%s</u></h3>", channel.Name))
+			buffer.WriteString("<br>")
+			for i := len(history.Messages) - 1; i >= 0; i-- {
+				m := history.Messages[i]
+				buffer.WriteString(fmt.Sprintf("<strong>%s: </strong>", userList[fmt.Sprintf("<@%s>", m.User)]))
+				buffer.WriteString(ReplaceMentionUser(userList, m.Text))
+				buffer.WriteString("<br>")
+			}
+		}
+	}
+
+	// Send html content to mailing list
+	return SendGridEmail(c, DigestTitle(), string(buffer.Bytes()))
+}
+
+func Users(api *slack.Client) (map[string]string, error) {
+	users, err := api.GetUsers()
+	if err != nil {
+		return nil, err
+	}
+	var userList = make(map[string]string)
+	for _, user := range users {
+		userList[fmt.Sprintf("<@%s>", user.ID)] = fmt.Sprintf("@%s", user.Name)
+	}
+	return userList, nil
+}
+
+func ReplaceMentionUser(userList map[string]string, text string) string {
+	var msg = text
+	for _, match := range re.FindAllString(msg, -1) {
+		msg = strings.ReplaceAll(msg, match, userList[match])
+	}
+	return msg
+}
+
+func SendGridEmail(c *Config, subject string, htmlContent string) string {
+	from := mail.NewEmail("Gobblin Slack Email Digest", c.From)
+	client := sendgrid.NewSendClient(c.SendgridToken)
+	to := mail.NewEmail("Apache Gobblin Dev", c.To)
+	message := mail.NewSingleEmail(from, subject, to, htmlContent, htmlContent)
+	response, err := client.Send(message)
+	if err != nil {
+		fmt.Println("Failed to send the mail via Sendgrid:  " + err.Error())
+		return "Failed to send mail via Sendgrid: " + err.Error()
+	}
+
+	if response.StatusCode >= 200 && response.StatusCode <= 204 {
+		msg := fmt.Sprintf("Daily digest sent with the title: `%s`\n", DigestTitle())
+		msg += fmt.Sprintf("StatusCode: `%d`\n", response.StatusCode)
+		fmt.Println(msg)
+		return msg
+	} else {
+		msg := fmt.Sprintf("Failed to send the mail via Sendgrid\n")
+		msg += fmt.Sprintf("StatusCode: `%d`\n", response.StatusCode)
+		msg += fmt.Sprintf("Body: ```%s```", response.Body)
+		fmt.Println(msg)
+		return msg
+	}
+}
---
+++ gobblin-bot.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-joe/cron"
+	joehttp "github.com/go-joe/http-server"
+	"github.com/go-joe/joe"
+	"github.com/go-joe/slack-adapter"
+)
+
+type GobblinBot struct {
+	*joe.Bot
+	Config *Config
+}
+
+type DailyDigestEvent struct {
+}
+
+type Config struct {
+	SlackAppToken     string
+	SlackBotUserToken string
+	From              string
+	To                string
+	SendgridToken     string
+	Port              string
+}
+
+func NewConfig() (*Config, error) {
+	config := &Config{
+		SlackAppToken:     os.Getenv("SLACK_APP_TOKEN"),
+		SlackBotUserToken: os.Getenv("SLACK_BOT_USER_TOKEN"),
+		From:              os.Getenv("FROM"),
+		To:                os.Getenv("TO"),
+		SendgridToken:     os.Getenv("SENDGRID_TOKEN"),
+		Port:              os.Getenv("PORT"),
+	}
+	fmt.Println(config)
+	if config.Port == "" {
+		config.Port = "80"
+	}
+	return config, nil
+}
+
+func main() {
+	config, err := NewConfig()
+	if err != nil {
+		log.Fatal("Failed to load config: ", err)
+	}
+	modules := []joe.Module{
+		joehttp.Server(":" + config.Port),
+		// Schedule the daily digest cron job at 2:00:00 AM (UTC)
+		cron.ScheduleEvent("53 * * * * *", DailyDigestEvent{}),
+	}
+	if config.SlackAppToken != "" && config.SlackBotUserToken != "" {
+		modules = append(modules, slack.Adapter(config.SlackBotUserToken))
+	}
+
+	b := &GobblinBot{
+		Bot:    joe.New("Gobblin-bot", modules...),
+		Config: config,
+	}
+
+	// Register event handlers
+	b.Brain.RegisterHandler(b.HandleDailyDigestEvent)
+	b.Brain.RegisterHandler(b.HandleHTTP)
+	b.Respond("daily-digest", b.DailyDigest)
+	b.Respond("config", b.PrintConfig)
+	b.Respond("ping", Pong)
+	b.Respond("time", Time)
+
+	b.Say("daily-digest", "Gobblin bot is starting..")
+	err = b.Run()
+	if err != nil {
+		b.Logger.Fatal(err.Error())
+	}
+}
+
+func (b *GobblinBot) HandleDailyDigestEvent(evt DailyDigestEvent) {
+	responseMsg := RunDailyDigest(b.Config)
+	b.Say("daily-digest", responseMsg)
+}
+
+func (b *GobblinBot) DailyDigest(msg joe.Message) error {
+	responseMsg := RunDailyDigest(b.Config)
+	msg.Respond(responseMsg)
+	return nil
+}
+
+func (b *GobblinBot) PrintConfig(msg joe.Message) error {
+	fmt.Println("printconfig")
+	configMsg := fmt.Sprintf("From: `%s`\n", b.Config.From)
+	configMsg += fmt.Sprintf("To: `%s`\n", b.Config.To)
+	configMsg += fmt.Sprintf("SlackAppToken: `%s`\n", b.Config.SlackAppToken)
+	configMsg += fmt.Sprintf("SlackBotUserToken: `%s`\n", b.Config.SlackBotUserToken)
+	configMsg += fmt.Sprintf("SendgridToken: `%s`", b.Config.SendgridToken)
+	msg.Respond(configMsg)
+	return nil
+}
+
+func (b *GobblinBot) HandleHTTP(c context.Context, r joehttp.RequestEvent) {
+	if r.URL.Path == "/" {
+		fmt.Println("Gobblin bot is running..")
+	}
+}
+
+func Time(msg joe.Message) error {
+	loc, _ := time.LoadLocation("America/Los_Angeles")
+	t := time.Now()
+	timeMsg := fmt.Sprintf("Machine local time: `%s`\n", fmt.Sprint(t))
+	timeMsg += fmt.Sprintf("Machine local time (in PDT): `%s`", fmt.Sprint(t.In(loc)))
+	msg.Respond(timeMsg)
+	return nil
+}
+
+func Pong(msg joe.Message) error {
+	msg.Respond("PONG")
+	return nil
+}
